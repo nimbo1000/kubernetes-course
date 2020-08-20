32886bba5481954a7c9659f7fe3b951d66235f91
nimbo1000 <48392661+nimbo1000@users.noreply.github.com>
2020-08-20 17:28:57 +0300

    Create grader script

---
+++ kubernetes-01-create-cluster/grader.py
@@ -0,0 +1,21 @@
+#This is a template for the grader
+
+from onpit import Grader
+from os.path import join, exists
+
+class LabGrader(Grader):
+	@Grader.addStep(name='step1')
+	def step1(self, workingDir, inputCommand):
+		if inputCommand == "kubectl version":
+			return True
+
+	@Grader.addStep(name='step2')
+	def step2(self, workingDir, inputCommand):
+    clusterInfo = False
+    getNodes = False
+		if inputCommand == "kubectl cluster-info":
+			clusterInfo = True
+    if inputCommand == "kubectl get nodes":
+			getNodes = True
+		if clusterInfo and getNodes:
+			return True
