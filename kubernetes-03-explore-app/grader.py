9814540d9ddd49c402ca204f925fdf0742d105e9
nimbo1000 <48392661+nimbo1000@users.noreply.github.com>
2020-08-20 19:02:21 +0300

    Update grader.py

--- kubernetes-03-explore-app/grader.py
+++ kubernetes-03-explore-app/grader.py
@@ -15,22 +15,27 @@ class LabGrader(Grader):
 
 	@Grader.addStep(name='step2')
 	def step2(self, workingDir, inputCommand):
-		if inputCommand == "kubectl run nginx --image=nginx --port=80":
-			self.firstCommand2 = True
-		if inputCommand == "kubectl get pods":
-			self.secondCommand2 = True
-		if hasattr(self, 'firstCommand2') and hasattr(self, 'secondCommand2') and self.firstCommand2 and self.secondCommand2:
-			return True
-
-	@Grader.addStep(name='step3')
-	def step3(self, workingDir, inputCommand):
 		if inputCommand == "kubectl proxy &":
 			self.firstCommand3 = True
-		if inputCommand == "curl http://localhost:8001/version":
-			self.secondCommand3 = True
 		if inputCommand == """export POD_NAME=$(kubectl get pods -o go-template --template '{{range .items}}{{.metadata.name}}{{"\\n"}}{{end}}') && echo Name of the Pod: $POD_NAME""":
 			self.thirdCommand3 = True
 		if inputCommand == "curl http://localhost:8001/api/v1/namespaces/default/pods/$POD_NAME/proxy/":
 			self.fourthCommand3 = True
-		if hasattr(self, 'firstCommand3') and hasattr(self, 'secondCommand3') and hasattr(self, 'thirdCommand3') and hasattr(self, 'fourthCommand3') and self.firstCommand3 and self.secondCommand3 and self.thirdCommand3 and self.fourthCommand3:
+		if hasattr(self, 'firstCommand3') and hasattr(self, 'thirdCommand3') and hasattr(self, 'fourthCommand3') and self.firstCommand3 and self.thirdCommand3 and self.fourthCommand3:
+			return True
+
+	@Grader.addStep(name='step3')
+	def step3(self, workingDir, inputCommand):
+		if inputCommand == "kubectl logs $POD_NAME":
+			return True
+
+	@Grader.addStep(name='step4')
+	def step4(self, workingDir, inputCommand):
+		if inputCommand == "kubectl exec $POD_NAME env":
+			self.firstCommand4 = True
+		if inputCommand == "kubectl exec -ti $POD_NAME bash":
+			self.thirdCommand4 = True
+		if inputCommand == "curl localhost:80":
+			self.fourthCommand4 = True
+		if hasattr(self, 'firstCommand4') and hasattr(self, 'thirdCommand4') and hasattr(self, 'fourthCommand4') and self.firstCommand4 and self.thirdCommand4 and self.fourthCommand4:
 			return True
