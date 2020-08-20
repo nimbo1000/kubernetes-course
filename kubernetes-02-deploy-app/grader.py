423028c80f2a19c9571175f5a764e51d6112c0a1
nimbo1000 <48392661+nimbo1000@users.noreply.github.com>
2020-08-20 18:39:27 +0300

    Update grader.py

--- kubernetes-02-deploy-app/grader.py
+++ kubernetes-02-deploy-app/grader.py
@@ -28,11 +28,15 @@ class LabGrader(Grader):
 			self.secondCommand3 = True
 		print(inputCommand)
 		print("""export POD_NAME=$(kubectl get pods -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}') && echo Name of the Pod: $POD_NAME""")
-		if inputCommand == """export POD_NAME=$(kubectl get pods -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}') && echo Name of the Pod: $POD_NAME""":
+		if inputCommand == """export POD_NAME=$(kubectl get pods -o go-template --template '{{range .items}}{{.metadata.name}}{{"\\n"}}{{end}}') && echo Name of the Pod: $POD_NAME""":
 			self.thirdCommand3 = True
 			self.fourthCommand3 = True
 		if inputCommand == "curl http://localhost:8001/api/v1/namespaces/default/pods/$POD_NAME/proxy/":
 			self.thirdCommand3 = True
 			self.fourthCommand3 = True
+		print(firstCommand3)
+		print(secondCommand3)
+		print(thirsCommand3)
+		print(fourthCommand3)
 		if hasattr(self, 'firstCommand3') and hasattr(self, 'secondCommand3') and hasattr(self, 'thirdCommand3') and hasattr(self, 'fourthCommand3') and self.firstCommand3 and self.secondCommand3 and self.thirdCommand3 and self.fourthCommand3:
 			return True
